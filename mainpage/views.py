45255a0104829f2755350fe8585347c89719f204
Manan Parmar <54072015+mananv1791@users.noreply.github.com>
2021-06-23 16:17:44 +0530

    Update views.py

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 def Homepage(request):
     return HttpResponseRedirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
