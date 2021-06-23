db962656195bd95d00cddd3efde8f5acb0b23918
Manan Parmar <54072015+mananv1791@users.noreply.github.com>
2021-06-23 16:25:21 +0530

    Final_Final_Final

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 def Homepage(request):
+    return HttpResponse("Hello World")
+
+
+def Homepage1(request):
     return HttpResponseRedirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
